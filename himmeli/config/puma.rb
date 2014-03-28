beda099337f62755fe4e8037e8e7337c7b293ead
teemujonkkari <teemu.jonkkari@gmail.com>
2014-03-28 17:15:42 +0200

    puma config

---
+++ himmeli/config/puma.rb
@@ -0,0 +1,5 @@
+pidfile "/home/amc/github-repos/himmeli/himmeli/tmp/puma/pid"
+state_path "/home/amc/github-repos/himmeli/himmeli/tmp/puma/state"
+bind 'tcp://127.0.0.1:3002'
+environment "production"
+activate_control_app
