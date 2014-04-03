d642c988cd0e02969938895e1702dcd2e596c1e5
teemujonkkari <teemu.jonkkari@gmail.com>
2014-04-03 11:30:54 +0300

    email changed to username in login

--- himmeli/db/schema.rb
+++ himmeli/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401123055) do
+ActiveRecord::Schema.define(version: 20140403081103) do
 
   create_table "events", force: true do |t|
     t.integer  "person_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20140401123055) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
